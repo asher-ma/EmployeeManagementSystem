a25342761704d2c0c9be0e6b290b71a714092db9
asher-ma <asherhmangel@gmail.com>
2026-04-19 08:24:22 +0000

    Added test cases

--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@ using namespace std;
 #include "include/comissionEmployee.h"
 
 void main (){
-    vector<Employee*> employeeVector;
+    vector<Employee*> employees;
 
+    employees.push_back(new SalariedEmployee("Alice", 101, 1200));
+    employees.push_back(new HourlyEmployee("Bob", 102, 15, 45));
+    employees.push_back(new ComissionEmployee("Marley", 103, 500, 3, 150));
+
+    for (int i = 0; i < employees.size(); i++){
+        employees[i]->display();
+    }
 }
