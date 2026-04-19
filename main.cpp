34fe999d7d6b20701060fb6e8297c5be57af9b7a
asher-ma <asherhmangel@gmail.com>
2026-04-19 08:33:36 +0000

    Changed to range based for loop

--- main.cpp
+++ main.cpp
@@ -7,14 +7,15 @@ using namespace std;
 #include "include/hourlyEmployee.h"
 #include "include/comissionEmployee.h"
 
-void main (){
+int main (){
     vector<Employee*> employees;
 
     employees.push_back(new SalariedEmployee("Alice", 101, 1200));
     employees.push_back(new HourlyEmployee("Bob", 102, 15, 45));
     employees.push_back(new ComissionEmployee("Marley", 103, 500, 3, 150));
 
-    for (int i = 0; i < employees.size(); i++){
-        employees[i]->display();
+    for (const Employee* emp : employees){
+        emp->display();
+        cout << "Weekly Pay: " << emp->calculatePay() << endl;
     }
 }
